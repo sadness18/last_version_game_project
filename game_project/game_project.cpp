8a6c038a8848af8f4f79f982caf1bba830c95276
Eleyd Tselykovskaya <Dessirdes@gmail.com>
2019-12-23 12:39:26 +0300

    full map

--- game_project/game_project.cpp
+++ game_project/game_project.cpp
@@ -14,6 +14,7 @@ protected:
 	float x, y, dx, dy; //позиция по (x, y); скорость по (dx, dy);
 	float sweem, speed, movetimer; //эффект плавания; скорость; счетчик для разного
 	int health, w, h; //здоровье; ширина спрайта; высота спрайта
+	int count_gametime; //счетчик времени игры
 	bool life; //игрок жив или нет
 	Texture texture; //текстура
 	Sprite sprite; //спрайт
@@ -22,7 +23,7 @@ public:
 	entity(Image& image, float X, float Y, int W, int H, float Dx, float Dy, String Name) //конструктор с параметрами
 	{
 		x = X; y = Y; w = W; h = H; name = Name; dx = Dx; dy = Dy;
-		movetimer = 0; health = 100; sweem = 0;
+		movetimer = 0; health = 100; sweem = 0; count_gametime = 0;
 		life = true;
 		texture.loadFromImage(image);
 		sprite.setTexture(texture);
@@ -117,6 +118,22 @@ public:
 	{
 		return name;
 	}
+	bool get_life()
+	{
+		return life;
+	}
+	int get_health()
+	{
+		return health;
+	}
+	void inc_count_gametime()
+	{
+		count_gametime++;
+	}
+	int get_count_gametime()
+	{
+		return count_gametime;
+	}
 };
 
 class enemy :public entity //дочерний класс (враг)
@@ -180,6 +197,15 @@ public:
 
 bool is_game(RenderWindow& window) //ф-ия is_game, принимающая в качестве параметра ссылку на объект окно из ф-ии main. Там же она и вызывается
 {
+	View view; //объект камера.
+	view.reset(FloatRect(64, 64, 1920, 1080)); //инициализация камеры. Поставить камеру, начиная с точки 64, 64, размер камеры 1920х1080
+
+	Font font; //объект шрифт
+	font.loadFromFile("18386.ttf"); //выбираем файл шрифта
+	Text text("", font, 20); //объект текст (строка, шрифт, размер)
+	text.setOutlineColor(Color::Black); //задать цвет тексту (внешняя линия)
+	text.setFillColor(Color::Black); //задать цвет тексту (заливка)
+
 	Image map_image; //объект изображение (для карты)
 	map_image.loadFromFile("images/map.png"); //загружаем картинку элементов карты
 	Texture t_map; //объект текстура
@@ -201,6 +227,9 @@ bool is_game(RenderWindow& window) //ф-ия is_game, принимающая в 
 	player p(heroimage, 64, 64, 60, 42, 0.15, 0, "player_1"); //создаем персонажа (объект подкласса player)
 
 	Clock clock; //переменная времени
+	int minutes = 0; //кол-во минут, прошедших с момента начала игры
+	int seconds = 0; //кол-во секунд, которые переводятся в минуты, когда достигают 60
+	int save_time = 0; //для сохранения времени
 
 	  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	 ////////////////////////////////////////////////////ПОКА ОТКРЫТО ОКНО////////////////////////////////////////////////////
@@ -212,6 +241,18 @@ bool is_game(RenderWindow& window) //ф-ия is_game, принимающая в 
 		time = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
 		clock.restart(); //перезагружает время (обнуляет)
 		time = time / 800; //скорость игры. Для справки (напоминалка) --> 1 000 000 мкс (1 с) / 800 = 1250 мкс (1 с)
+
+		if (p.get_life()) //если игрок жив
+		{
+			save_time += time; //в переменной save_time накапливаем микросекунды
+			if (save_time >= 1250) //если накопилось 1250 мкс или больше (1 секунда)
+			{
+				p.inc_count_gametime(); //инкрементируем переменную count_gametime. Она и накапливает в себе игровое время
+				save_time = 0; //обнуляем save_time, чтобы снова накапливать в ней 1250 мкс. И так постоянно, пока выполняются условия
+			}
+		}
+		minutes = floor(p.get_count_gametime() / 60); //для вывода на экран времени в минутах (сам вывод в конце, где отрисовка объектов)
+		seconds = p.get_count_gametime() % 60; //для вывода на экран времени в секундах (до 60) (сам вывод в конце, где отрисовка объектов)
 
 		Event event; //объект класса event
 		while (window.pollEvent(event)) //пока что-то там event
@@ -227,6 +268,8 @@ bool is_game(RenderWindow& window) //ф-ия is_game, принимающая в 
 
 		p.update(time); //метод update класса player
 
+		window.setView(view); //устанавливаем камеру в нужное место (сдвигаем на 64 пикселя вниз и вправо, чтобы не было видно границ карты)
+
 		window.clear(); //отчищаем окно от предыдущего кадра
 
 		for (int i = 0; i < height_map; i++) //циклы для прорисовки карты
@@ -235,10 +278,32 @@ bool is_game(RenderWindow& window) //ф-ия is_game, принимающая в 
 			{
 				if (TileMap[i][j] == ' ') s_map.setTextureRect(IntRect(0, 0, 64, 64)); //обычная вода
 				if (TileMap[i][j] == '0') s_map.setTextureRect(IntRect(64, 0, 128, 64)); //водоросли(препятствие)
+				if (TileMap[i][j] == 'd') s_map.setTextureRect(IntRect(128, 0, 192, 64)); //грязь внизу (препятствие)
+				if (TileMap[i][j] == 'k') s_map.setTextureRect(IntRect(192, 0, 256, 64)); //осьминог (декор)
+				if (TileMap[i][j] == 'c') s_map.setTextureRect(IntRect(0, 64, 64, 128)); //краб (декор)
+				if (TileMap[i][j] == 'y') s_map.setTextureRect(IntRect(64, 64, 128, 128)); //утка (декор)
+				if (TileMap[i][j] == 'u') s_map.setTextureRect(IntRect(128, 64, 192, 128)); //улитка горизонтальная (декор)
+				if (TileMap[i][j] == 'U') s_map.setTextureRect(IntRect(192, 64, 256, 128)); //улитка вертикальная (декор)
+				if (TileMap[i][j] == '1') s_map.setTextureRect(IntRect(0, 128, 64, 192)); //папоротник средний (декор)
+				if (TileMap[i][j] == '2') s_map.setTextureRect(IntRect(64, 128, 128, 192)); //папоротник жирный (декор)
+				if (TileMap[i][j] == '3') s_map.setTextureRect(IntRect(128, 128, 192, 192)); //папоротник тонкий (декор)
+				if (TileMap[i][j] == '4') s_map.setTextureRect(IntRect(192, 128, 256, 192)); //папоротник кривой (декор)
 
 				s_map.setPosition(j * 64, i * 64); //расставляем блоки текстур
 				window.draw(s_map); //рисуем карту
 			}
+		}
+
+		ostringstream playerhealth, minutesstring, secondsstring, counteat, countbuff, countstun;
+		//создаем специальные строки для вывода их на экран (обычные выводятся в консоль) и помещаем в них обычные строки
+		playerhealth << p.get_health(); //строка числового вывода здоровья (только для тестов)
+		minutesstring << minutes; //строка подсчета минут игры
+		secondsstring << seconds; //строка подсчета секунд игры (до 60 секунд)
+		if (p.get_life()) //если персонаж жив
+		{
+			text.setString(L"Здоровье: " + playerhealth.str() + L"\nВремя игры: " + minutesstring.str() + L" минут(ы) " + secondsstring.str() + L" секунд(ы) "); //выводим на экран
+			text.setPosition(1500, 64); //задаем позицию текста на экране
+			window.draw(text); //рисуем текст
 		}
 
 		for (it = entities.begin(); it != entities.end(); ++it) { window.draw((*it)->get_sprite()); } //рисуем врагов
@@ -257,7 +322,7 @@ void restart_func(RenderWindow & window) //промежуточная ф-ия д
 
 int main() //основная ф-ия (всегда вызывается первой в программе)
 {
-	RenderWindow window(VideoMode(640, 480), "GlassHouse"/*, Style::Fullscreen*/); //создаем окно (разрешение, название, на полный экран)
+	RenderWindow window(VideoMode(/*640*/1920, /*480*/1080), "GlassHouse", Style::Fullscreen); //создаем окно (разрешение, название, на полный экран)
 	restart_func(window); //все остальное
 	return 0; //вот и сказочке конец! А кто работал над ней, пойдет спать... Короче возвращаем 0, если все хорошо. (хоть где-то 0 - это хорошо)
 }
